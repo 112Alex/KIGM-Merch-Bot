d7d37b4ed145a4c9ea5b8c0e014d722d8606879b
sasha <sasasuhinin170@gmail.com>
2024-10-21 18:52:27 +0300

    обработка ошибок админа

--- handlers/admin_private.py
+++ handlers/admin_private.py
@@ -11,27 +11,73 @@ from keybds.reply import *
 admin_router = Router()
 admin_router.message.filter(ChatTypeFilter(["private"]), IsAdmin())
 
-class AdminMenu(StatesGroup): #NOTE FSM для админки
-    set_action = State()
+#COMMENT код ниже для FSM
+class AdminMenu(StatesGroup):
+    menu = State()
     set_event = State()
     zaglushka = State()
 
-@admin_router.message(StateFilter(None), Command("admin_menu"))
+    texts = {
+        'AdminMenu:menu': 'Выберите действие',
+    }
+
+#COMMENT отменить действие (сбросить состояние)
+#FIXME Исправить и протестировать
+@admin_router.message(StateFilter('*'), Command("отмена"))
+@admin_router.message(StateFilter('*'), F.text.casefold() == "отмена")
+async def cancel_handler(message: types.Message, state: FSMContext) -> None:
+
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    await state.clear()
+    await message.answer("Действия отменены", reply_markup=ADMIN_KB)
+
+#COMMENT вернуться на шаг назад (на предыдущее состояние)
+#BUG меню не обрабатывается. Скорее всего из-за очистки состояния после выбора мероприятия
+@admin_router.message(StateFilter('*'), Command("назад"))
+@admin_router.message(StateFilter('*'), F.text.casefold() == "назад")
+async def back_step_handler(message: types.Message, state: FSMContext) -> None:
+
+    current_state = await state.get_state()
+
+    if current_state == AdminMenu.menu: #TODO
+        await message.answer('Предыдущего шага нет, или введите название товара или напишите "отмена"')
+        return
+
+    previous = None
+    for step in AdminMenu.__all_states__: #TODO
+        if step.state == current_state:
+            await state.set_state(previous)
+            await message.answer(f"Ок, вы вернулись к прошлому шагу \n {AdminMenu.texts[previous.state]}")
+            return
+        previous = step
+
+
+@admin_router.message(StateFilter('*'), Command("admin_menu"))
 async def admin_menu(msg: types.Message, state: FSMContext):
     await msg.answer(text='меню:', reply_markup=ADMIN_KB)
-    await state.set_state(AdminMenu.set_action)
+    await state.set_state(AdminMenu.menu)
 
-@admin_router.callback_query(AdminMenu.set_action, F.data == 'add_event')
+@admin_router.message(AdminMenu.menu)
+async def add_event_handle(msg: types.Message, state: FSMContext):
+    await msg.answer(text='Вы ввели некорректные данные. Выберите действие:')
+
+
+@admin_router.callback_query(AdminMenu.menu, F.data == 'add_event')
 async def add_event_handle(callback: CallbackQuery, state: FSMContext):
-    await state.update_data(set_action = callback.message.text)
+    await state.update_data(menu = callback.message.text)
     await callback.message.answer(text='Выберите действие:', reply_markup=ADD_EVENT_KEYBOARD)
     await state.set_state(AdminMenu.set_event)
+
 
 @admin_router.message(AdminMenu.set_event, F.text.lower() == 'волонтёрское благотворительное мероприятие в колледже')
 async def vbmvk_handler(msg: types.Message, state: FSMContext):
     await state.update_data(set_event = msg.text)
     await msg.answer(text="мероприятие выбрано", reply_markup=types.ReplyKeyboardRemove())
     await state.clear()
+
 
 
 #TODO Дописать остальные админ-функции
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,8 +1,9 @@
 from aiogram import F, types, Router
 from aiogram.types import CallbackQuery
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart, Command, StateFilter
 from filters.chat_types import ChatTypeFilter
 from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 from keybds.reply import *
 from common.variables import *
@@ -27,21 +28,31 @@ async def chek_id(msg: types.Message):
 async def menu(msg: types.Message):
     await msg.answer(text='выберите действие:', reply_markup=MENU_KEYBOARD)
 
+
 #TODO сделать авторизацию и регистрацию 
 #[x] FSM 
 
-@user_private_router.callback_query(F.data == 'auth')
-async def authorization(callback: CallbackQuery):
-    await callback.message.answer('Введите логин:')
 
+class Auth(StatesGroup): #NOTE класс состояния регистрации
+    login = State()
 
-class auth(StatesGroup): # FSM для регистрации
-    authorization = State()
-
-class reg(StatesGroup): # FSM для авторизации
+class Reg(StatesGroup): #NOTE класс состояния авторизации
     registration = State()
 
+class UserState(StatesGroup): #NOTE класс обычного состояния пользователя
+    user_state = State()
 
-@user_private_router.message(F.text.lower() == 'user')
-async def auth_login_check(msg: types.Message):
+@user_private_router.callback_query(StateFilter('*'), F.data == 'auth')
+async def authorization(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer('Введите логин:')
+    await state.set_state(Auth.login)
+
+@user_private_router.message(Auth.login, F.text.lower() == 'user')
+async def auth_login_check(msg: types.Message, state: FSMContext):
+    await state.update_data(login=msg.text)
     await msg.answer(text='Введите пароль:')
+    await state.set_state(UserState.user_state)
+
+@user_private_router.message(UserState.user_state, F.text.lower() == 'Z')
+async def z(msg: types.Message, state: FSMContext):
+    await msg.answer(text=msg.chat.username)

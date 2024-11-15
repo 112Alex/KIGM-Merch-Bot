94a113df07b7591a86d5b65a6afd8f2156f610b3
sasha <sasasuhinin170@gmail.com>
2024-11-16 00:17:03 +0300

    main.py orm

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from aiogram import Dispatcher
 from aiogram import types
 
 from dotenv import find_dotenv, load_dotenv
+
 load_dotenv(find_dotenv())
+
+from middlewares.db import DataBaseSession
+
+from database.engine import create_db, drop_db, session_maker
 
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
@@ -24,8 +29,23 @@ dp.include_router(user_private_router)
 dp.include_router(user_group_router)
 dp.include_router(admin_router)
 
+async def on_startup(bot):
+    run_param = False
+    if run_param:
+        await drop_db()
+    await create_db()
+
+async def on_shutdown(bot):
+    print('бот лёг')
+
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+    
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
+
     logging.basicConfig(level=logging.INFO)
+
     await bot.delete_webhook(drop_pending_updates=True)
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
